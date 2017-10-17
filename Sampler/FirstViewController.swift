f0f2b59a2f094e66c969e74bb83a87132b2b42be
Sapna Chandiramani <schandiramani@openvpn-client.vpn.penchecks.com>
2017-10-17 16:11:40 -0700

    Initial Commit

---
+++ Sampler/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Sampler
+//
+//  Created by Sapna Chandiramani on 10/17/17.
+//  Copyright © 2017 Sapna Chandiramani. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
